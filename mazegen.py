c061eee6b680a8cac7f63d8f56ea078236e16f7a
Eeo Jun <141bytes@gmail.com>
2017-04-05 16:02:59 +0800

    update maze gen

--- mazegen.py
+++ mazegen.py
@@ -1,58 +1,77 @@
-from pprint import pprint
-from collections import deque
+from collections import namedtuple
 from random import choice, randint
 from mt import make_grid
 
 
-def bounded_pos(pos, grid):
-    x, y = pos
-    if 0 <= x < len(grid[0]) and 0 <= y < len(grid):
-        return pos
-    return None
+Pos = namedtuple('Pos', ['x', 'y'])
 
 
-def legal_deltas(pos, grid, deltas):
+def width(maze):
+    return len(maze[0])
+
+
+def height(maze):
+    return len(maze)
+
+
+def set(maze, p, v):
+    maze[p.y][p.x] = v
+
+
+def get(maze, p):
+    return maze[p.y][p.x]
+
+
+def is_legal(maze, pos):
+    return 0 <= pos.x < width(maze) and 0 <= pos.y < height(maze)
+
+
+def dist2(pos):
     x, y = pos
-    for dx, dy in deltas:
-        coord = (x + dx, y + dy)
-        coord = bounded_pos(coord, grid)
-        if coord is None:
-            continue
-        yield coord
+    yield Pos(x + 2, y)
+    yield Pos(x - 2, y)
+    yield Pos(x, y + 2)
+    yield Pos(x, y - 2)
+
+
+def frontier(maze, pos):
+    for p in dist2(pos):
+        if is_legal(maze, p) and get(maze, p) == 0:
+            yield p
+
+
+def neighbors(maze, pos):
+    for p in dist2(pos):
+        if is_legal(maze, p) and get(maze, p) == 1:
+            yield p
+
+
+def between(A, B):
+    x = min(A.x, B.x) + 1 if A.x != B.x else A.x
+    y = min(A.y, B.y) + 1 if A.y != B.y else A.y
+    return Pos(x, y)
 
 
 def gen(width, height):
-    maze = make_grid(width, height-1, fill=0)
-    head = randint(0, width-1)
-    maze[0][head] = 1
+    maze = make_grid(width, height - 1, fill=0)
+    seed = Pos(randint(0, width - 1), 0)
+    set(maze, seed, 1)
+    front = list(frontier(maze, seed))
+    while front:
+        idx = randint(0, len(front) - 1)
+        cell = front.pop(idx)
+        set(maze, cell, 1)
+        peer = choice(list(neighbors(maze, cell)))
+        set(maze, between(peer, cell), 1)
+        front.extend(frontier(maze, cell))
 
-    frontier = [(head, 0)]
-    visited = set(frontier)
-    while frontier:
-        pos = frontier.pop()
-        points = legal_deltas(pos, maze, [
-            (+0, -1),
-            (+0, +1),
-            (-1, +0),
-            (+1, +0),
-        ])
-        for point in points:
-            if point in visited:
-                continue
-            visited.add(point)
-            x, y = point
-            if y == 0:
-                continue
-            value = choice((0, 1))
-            if all(v == 0 for v in maze[y]):
-                value = 1
-            maze[y][x] = value
-            if value == 1:
-                frontier.append(point)
+    entrance = make_entrance(maze[0])
+    exit = make_entrance(maze[-1])
+    return [entrance] + maze + [exit]
 
-    # add the exit
-    indices = [i for i, v in enumerate(maze[-1]) if v == 1]
-    last = [0] * width
-    last[choice(indices)] = 1
-    maze.append(last)
-    return maze
+
+def make_entrance(reference):
+    idx = choice([i for i, v in enumerate(reference) if v == 1])
+    row = [0] * len(reference)
+    row[idx] = 1
+    return row
