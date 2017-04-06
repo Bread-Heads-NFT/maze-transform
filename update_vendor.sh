2e4824520fe512ab6fc02e5d1307220589b4428f
Eeo Jun <141bytes@gmail.com>
2017-04-06 16:30:21 +0800

    vendored png -> 0.18.0

--- update_vendor.sh
+++ update_vendor.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget 'https://raw.githubusercontent.com/drj11/pypng/dacbebd36c2de913aae85500ce47bbb9a45ebff6/code/png.py' -O libmaze/vendor/png.py
+wget 'https://raw.githubusercontent.com/drj11/pypng/f5c4c76d81093b6c3f39f83b203f6832c496c110/code/png.py' -O libmaze/vendor/png.py
 wget 'https://raw.githubusercontent.com/docopt/docopt/944641a7357376a43151bd4a3ca44f9c07c4d1ae/docopt.py' -O libmaze/vendor/docopt.py
